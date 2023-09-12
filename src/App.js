ff0116d4abddc9082e486b564a0b7a30c1b78325
alisahibmammadov <ali.yek.10@mail.ru>
2023-09-12 16:49:38 +0400

    msg

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-
-
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
+  const [menu,setMenu] = useState(false)
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route/>
-      </Routes>
+        <Navbar menu={menu} setMenu={setMenu}/>
+        
+        <Routes>
+          <Route />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-// import WebLogo from '../../assets/img/logo/websiteLogo.png'
-import '../../assets/styles/navbar/style.scss'
-function Navbar() {
-  return (
-    <nav className='container navbar'>
-        {/* <img src={WebLogo} alt="WebSite Logo" /> */}
+import React from "react";
+import WebLogo from "../../assets/img/logo/websiteLogo.png";
+import "../../assets/styles/navbar/Navbar.scss";
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
 
+function Navbar({menu,setMenu}) {
+  const handleClick = () =>{  
+    setMenu(!menu)
+  }
+  return (
+    <nav className="container main-navbar">
+      <Link to='/'>
+      <img src={WebLogo} alt="WebSite Logo" className="web-logo"/>
+      </Link>
+
+      <div className="navbar__menu-course">
+        <span className="bars-icon">
+          <i className="fa-solid fa-bars-staggered"></i>
+        </span>
+        <Link className="course-link" to="/">Kurslar</Link>
+      </div>
+
+      <div className="navbar__wishlist">
+        <span className="heart-icon"><i className="fa-regular fa-heart"></i></span>
+        <Link className="wishlist-link" to='/'>İstək siyahısı</Link>
+      </div>
+
+      <div className="navbar__btn-boxes">
+        <Button className="login" variant="link"><i className="fa-solid fa-arrow-right-to-bracket"></i> Daxil ol</Button>
+        <Button className="signup" variant="link"><i className="fa-regular fa-pen-to-square"></i> Qeydiyyatdan keç</Button>
+      </div>
+      <div className="menu-icon-box" onClick={handleClick}>
+        <i className="fa-solid fa-bars-staggered"></i>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
