c0dca1bf3a7fca188ab033a08d9a9ab31a320241
alisahibmammadov <ali.yek.10@mail.ru>
2023-09-12 13:19:49 +0400

    msg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react'
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
