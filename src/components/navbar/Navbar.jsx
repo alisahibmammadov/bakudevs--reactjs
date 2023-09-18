54b94515641d557d9134d916e4ab98583ae22f55
alisahibmammadov <ali.yek.10@mail.ru>
2023-09-18 16:55:37 +0400

    Navbar bg function created but uncomplated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WebLogo from "../../assets/img/logo/websiteLogo.png";
 import "../../assets/styles/navbar/Navbar.scss";
 import { Link } from "react-router-dom";
@@ -6,41 +6,56 @@ import SignupBtn from "../Buttons/SignupBtn";
 import LoginBtn from "../Buttons/LoginBtn";
 
 function Navbar({ menu, setMenu }) {
-  const handleClick = () => {
-    setMenu(!menu);
-  };
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  function handleScroll() {
+    if (window.screenY > 0) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="container main-navbar">
-      <Link to="/">
-        <img src={WebLogo} alt="WebSite Logo" className="web-logo" />
-      </Link>
+    <section className={`section-navbar ${isScrolled ? '.bg-white' : ""}`}>
+      <nav className="container main-navbar">
+        <Link to="/">
+          <img src={WebLogo} alt="WebSite Logo" className="web-logo" />
+        </Link>
 
-      <div className="navbar__menu-course">
-        <span className="bars-icon">
+        <div className="navbar__menu-course">
+          <span className="bars-icon">
+            <i className="fa-solid fa-bars-staggered"></i>
+          </span>
+          <Link className="course-link" to="/">
+            Kurslar
+          </Link>
+        </div>
+
+        <div className="navbar__wishlist">
+          <span className="heart-icon">
+            <i className="fa-regular fa-heart"></i>
+          </span>
+          <Link className="wishlist-link" to="/">
+            İstək siyahısı
+          </Link>
+        </div>
+
+        <div className="navbar__btn-boxes">
+          <LoginBtn />
+          <SignupBtn />
+        </div>
+        <div className="menu-icon-box" onClick={handleClick}>
           <i className="fa-solid fa-bars-staggered"></i>
-        </span>
-        <Link className="course-link" to="/">
-          Kurslar
-        </Link>
-      </div>
-
-      <div className="navbar__wishlist">
-        <span className="heart-icon">
-          <i className="fa-regular fa-heart"></i>
-        </span>
-        <Link className="wishlist-link" to="/">
-          İstək siyahısı
-        </Link>
-      </div>
-
-      <div className="navbar__btn-boxes">
-        <LoginBtn />
-        <SignupBtn />
-      </div>
-      <div className="menu-icon-box" onClick={handleClick}>
-        <i className="fa-solid fa-bars-staggered"></i>
-      </div>
-    </nav>
+        </div>
+      </nav>
+    </section>
   );
 }
 
