c81a25b3d6bbdbf20408b24ecba6aa662e436325
alisahibmammadov <ali.yek.10@mail.ru>
2023-09-19 14:43:04 +0400

    Home Advertising Page created but compated

---
+++ src/components/HomePage/HomeAdvertisingPage.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import AdvertisingBoy from "../../assets/img/HomePage/HomeAdvertisingBoy.png";
+import { Container, Row, Col } from "react-bootstrap";
+import "../../assets/styles/HomePage/HomeAdvertisingPage.scss";
+
+function HomeAdvertisingPage() {
+  return (
+    <main className="main-advertising">
+      <Container>
+        <Row className="advertising-head-row">
+          <Col>
+            <div className="advertising-head">
+              <h2>BakuDevs ilə istədiyiniz biliklərə sahib olun!</h2>
+              <p>
+                Nə üçün tədrisinizi məhz bizdə keçməlisiniz? Bunun üçün ən azı
+                dörd səbəb var:
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row className="advertising-cards-row">
+          <Col md={6}>
+            <div className="advertising-cards-box">
+              <div className="advertising-card">
+                <i class="fa-solid fa-money-bill"></i>
+                <h3>Sərfəli qiymət</h3>
+                <p>
+                  Ayda cəmi 10 AZN ödəniş etməklə saytdımızdaki bütün kurslar
+                  sizin üçün tam açıq olacaq!
+                </p>
+              </div>
+              <div className="advertising-card">
+                <i class="fa-solid fa-money-bill"></i>
+                <h3>Sərfəli qiymət</h3>
+                <p>
+                  Ayda cəmi 10 AZN ödəniş etməklə saytdımızdaki bütün kurslar
+                  sizin üçün tam açıq olacaq!
+                </p>
+              </div>
+              <div className="advertising-card">
+                <i class="fa-solid fa-money-bill"></i>
+                <h3>Sərfəli qiymət</h3>
+                <p>
+                  Ayda cəmi 10 AZN ödəniş etməklə saytdımızdaki bütün kurslar
+                  sizin üçün tam açıq olacaq!
+                </p>
+              </div>
+              <div className="advertising-card">
+                <i class="fa-solid fa-money-bill"></i>
+                <h3>Sərfəli qiymət</h3>
+                <p>
+                  Ayda cəmi 10 AZN ödəniş etməklə saytdımızdaki bütün kurslar
+                  sizin üçün tam açıq olacaq!
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col lg={6} style={{background:"red"}}>
+            <div className="advertising-img">
+              <img src={AdvertisingBoy} alt="" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  );
+}
+
+export default HomeAdvertisingPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePageHead from "../components/HomePage/HomePageHead";
 import HomeCoursesPage from "../components/HomePage/HomeCoursesPage";
+import HomeAdvertisingPage from "../components/HomePage/HomeAdvertisingPage";
 
 function HomePage() {
   return (
     <div >
       <HomePageHead />
       <HomeCoursesPage />
+      <HomeAdvertisingPage/>
     </div>
   );
 }
