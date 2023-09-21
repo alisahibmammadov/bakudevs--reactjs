ef09711f6ccb097afefc219cffe7d4803cf1fe42
alisahibmammadov <ali.yek.10@mail.ru>
2023-09-21 16:55:02 +0400

    PagesBg metods created

--- src/components/PagesBg.jsx
+++ src/components/PagesBg.jsx
@@ -2,38 +2,87 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "../assets/styles/PagesBg.scss";
 import { Link } from "react-router-dom";
-
 function PagesBg({ pages }) {
-  console.log(pages);
+  const data = ["Kurslar", "Haqqımızda", "Əlaqə"];
+  let dataLar =  data.filter(item=>item === pages)
+  console.log(dataLar);
   return (
     <main className="main-pages-bg">
-      <section>
+      <section style={pages === "Kurs" ? { padding: "49px 0" } : {}}>
         <Container className="pages-bg-container">
           <Row>
             <Col>
               <div className="pages-bg-head">
-                {pages === "courses" ? (
+                {pages === "Kurslar" ? (
                   <h2>Kurslar</h2>
-                ) : pages === "course" ? (
+                ) : pages === "Kurs" ? (
                   <h2>Android Program Development</h2>
-                ) : pages === "about" ? (
+                ) : pages === "Haqqımızda" ? (
                   <h2>Haqqımızda</h2>
-                ) : pages === "contact" ? (
+                ) : pages === "Əlaqə" ? (
                   <h2>Əlaqə</h2>
                 ) : null}
               </div>
             </Col>
           </Row>
-          <Row >
-            <Col >
-              <div className="pages-bg-links">
-                <Link to="/" className="pages-bg-home">Əsas səhifə</Link>
-                <ul>
-                  <li>
-                    <Link className="pages-bg-link" to="/courses">Kurslar</Link>
-                  </li>
-                </ul>
-              </div>
+          <Row>
+            <Col>
+            {
+              data.filter(item => item === pages).map(item=>(
+                <div className="pages-bg-links">
+                  <Link to="/" className="pages-bg-home">
+                    Əsas səhifə
+                  </Link>
+                  <ul>
+                    <li>
+                      <Link className="pages-bg-link" to={`/${item.toLowerCase()}`}>
+                        {item}
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              ))
+            }
+              {/* {pages === "courses" ? (
+                <div className="pages-bg-links">
+                  <Link to="/" className="pages-bg-home">
+                    Əsas səhifə
+                  </Link>
+                  <ul>
+                    <li>
+                      <Link className="pages-bg-link" to="/courses">
+                        {dataLar}
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : pages === "course" ? null : pages === "about" ? (
+                <div className="pages-bg-links">
+                  <Link to="/" className="pages-bg-home">
+                    Əsas səhifə
+                  </Link>
+                  <ul>
+                    <li>
+                      <Link className="pages-bg-link" to="/about">
+                        Haqqımızda
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : pages === "contact" ? (
+                <div className="pages-bg-links">
+                  <Link to="/" className="pages-bg-home">
+                    Əsas səhifə
+                  </Link>
+                  <ul>
+                    <li>
+                      <Link className="pages-bg-link" to="/about">
+                        Əlaqə
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : null} */}
             </Col>
           </Row>
         </Container>
--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -7,7 +7,7 @@ function CoursesPage() {
   const [course, setCourse] = useState(false);
   return (
     <Container fluid style={{ padding: "100px 0 0" }}>
-      <PagesBg pages="about" />
+      <PagesBg pages="Kurs" />
       <HomeCoursesPage course={course} />
     </Container>
   );
