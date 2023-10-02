a4b1c99cd39b904137808023e5962ad1cf988056
alisahibmammadov <ali.yek.10@mail.ru>
2023-10-02 20:39:49 +0400

    msg

--- src/pages/PrivateCabinet.jsx
+++ src/pages/PrivateCabinet.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import { Container,Row,Col } from 'react-bootstrap'
-import '../assets/styles/PrivateCabinet/PrivateCabinet.scss'
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "../assets/styles/PrivateCabinet/PrivateCabinet.scss";
 
 
 function PrivateCabinet() {
   return (
-    <Container className='private-cabinet-container'>
-        <Row>
-          <Col lg={4}>
-            <div className=''></div>
-          </Col>
-          <Col lg={8}></Col>
-        </Row>
+    <Container fluid className="private-cabinet-container">
+      <main className='container'>
+
+      <Row>
+        <Col lg={4}>
+          <div className="private-cabinet-links-boxes">
+            <header>
+              <h6>Şəxsi kabinet</h6>
+            </header>
+            <div className='btn-boxes'>
+              <Button><i className="fa-regular fa-user"></i>Profil</Button>
+              <Button><i className="fa-solid fa-plus"></i>Abunəlik</Button>
+              <Button><i className="fa-solid fa-receipt"></i>Qəbzlər</Button>
+              <Button><i className="fa-regular fa-heart"></i>İstək siyahısı</Button>
+              <Button><i className="fa-solid fa-arrow-right-from-bracket"></i>Çıxış</Button>
+            </div>
+          </div>
+        </Col>
+        <Col lg={8}>
+          <div className="private-cabinet-links-boxes"></div>
+        </Col>
+      </Row>
+      </main>
     </Container>
-  )
+  );
 }
 
-export default PrivateCabinet
+export default PrivateCabinet;
