ca88f7fcbdba6d1ff54eb8aee1a7e614eb1c6052
alisahibmammadov <ali.yek.10@mail.ru>
2023-10-04 13:34:12 +0400

    Recipts Part created but responsivenes uncomplated

--- src/pages/privateCabinet/parts/Receipts.jsx
+++ src/pages/privateCabinet/parts/Receipts.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import '../../../assets/styles/PrivateCabinet/parts/Receipts.scss'
+import React from "react";
+import "../../../assets/styles/PrivateCabinet/parts/Receipts.scss";
 
 function Receipts() {
   return (
-    <main className='private-receipts-container'></main>
-  )
+    <main className="private-receipts-container">
+      <div className="table">
+        <div className="table-head">
+          <h6>Qəbz ID</h6>
+          <h6>Abunə tarixi</h6>
+          <h6>Məbləğ</h6>
+        </div>
+        <div className="table-body">
+          <div className="body-row">
+            <p>
+              <i className="fa-solid fa-receipt"></i> 000000001
+            </p>
+            <p>12.02.2022</p>
+            <p>10 AZN</p>
+          </div>
+          <div className="body-row">
+            <p>
+              <i className="fa-solid fa-receipt"></i> 000000001
+            </p>
+            <p>12.02.2022</p>
+            <p>10 AZN</p>
+          </div>
+          <div className="body-row">
+            <p>
+              <i className="fa-solid fa-receipt"></i> 000000001
+            </p>
+            <p>12.02.2022</p>
+            <p>10 AZN</p>
+          </div>
+          <div className="body-row">
+            <p>
+              <i className="fa-solid fa-receipt"></i> 000000001
+            </p>
+            <p>12.02.2022</p>
+            <p>10 AZN</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default Receipts
+export default Receipts;
