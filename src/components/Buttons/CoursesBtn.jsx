41856b47a49a6eb557d1d716c8d5d152cf2eecc5
alisahibmammadov <ali.yek.10@mail.ru>
2023-10-05 19:40:19 +0400

    msg

--- src/components/Buttons/CoursesBtn.jsx
+++ src/components/Buttons/CoursesBtn.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../../assets/styles/Buttons/CoursesBtn.scss";
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function CoursesBtn() {
   return (
     <Button variant="link" className="courses-btn">
-      Kurslar <i className="fa-solid fa-chevron-right"></i>
+      <Link className="courses-btn" to='/courses' >Kurslar <i className="fa-solid fa-chevron-right"></i></Link>
     </Button>
   );
 }
